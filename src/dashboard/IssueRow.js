98a53e2086a326e7c6ba1853db8f7987f578ed09
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2016-06-06 12:09:40 +0300

    Beginning the share button

--- src/dashboard/IssueRow.js
+++ src/dashboard/IssueRow.js
@@ -6,17 +6,24 @@ export default class IssueRow extends React.Component {
         super(props);
     }
 
+    onClick(e) {
+        e.preventDefault();
+    }
+
     render() {
         // TODO Include year if year is different.
         let dateFormatted = this.props.timestamp ? moment(this.props.timestamp * 1000).format('D MMMM') : null;
         let link = Auth.addTokenToQuery(this.props.link);
         return (
-            <tr>
+            <tr className="issue-row">
                 <td className="issue-date">{dateFormatted}</td>
                 <td>
-                    <a href={link}>
-                        #{this.props.number} — {this.props.subject}
-                    </a>
+                    <header className="issue-row-header">
+                        <a href={link}>
+                            #{this.props.number} — {this.props.subject}
+                        </a>
+                    {/* <a className="btn btn-default issue-share" role="button" onClick={this.onClick.bind(this)}>Поделиться</a> */}
+                    </header>
                 </td>
             </tr>
         );

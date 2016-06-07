cd59c4bb517c993217fd8dae55cbc40b4c8e9be8
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2016-06-07 17:38:04 +0300

    Handling login form smoothly

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -4,8 +4,10 @@ import React from 'react';
 
 import './login.sass';
 
-const successStatus = 'success';
-const errorStatus = 'error';
+const status = {
+    success: 'success',
+    error: 'error'
+};
 
 export default class LoginForm extends React.Component {
     constructor(props) {
@@ -14,6 +16,7 @@ export default class LoginForm extends React.Component {
         this.state = {
             email: null,
             status: null,
+            progress: false,
             message: null
         };
     }
@@ -24,7 +27,12 @@ export default class LoginForm extends React.Component {
 
     onSubmit(e) {
         e.preventDefault();
+        this.refs.email.blur();
         let email = _.trim(this.state.email);
+
+        this.setState({
+            progress: true
+        });
 
         $.authorizedXHR({
             url: '/backend/token/' + email,
@@ -32,33 +40,41 @@ export default class LoginForm extends React.Component {
             cache: false
         }).then(() => {
             this.setState({
-                status: successStatus
+                status: status.success
             });
         }).fail((xhr) => {
             this.setState({
-                status: errorStatus,
+                status: status.error,
                 message: `${xhr.status} — ${xhr.statusText}`
+            });
+        }).done(() => {
+            this.setState({
+                progress: false
             });
         });
     }
 
     render() {
         let content;
-        if (this.state.status === errorStatus) {
+        if (this.state.status === status.error) {
             content = (
                 <p className="login-form-message login-form-error">
                     Ошибка при отправке письма:
                     <br/>
                     {this.state.message}
                 </p>);
-        } else if (this.state.status === successStatus) {
+        } else if (this.state.status === status.success) {
             content = <p className="login-form-message">Ссылка для входа отправлена на почту.</p>;
         } else {
+            let backdrop = (<div className="modal-backdrop fade in">
+                    <div className="spinner"></div>
+                </div>);
             content = (
                 <form className="form-inline login-form" onSubmit={this.onSubmit.bind(this)}>
+                    {this.state.progress ? backdrop : null}
                     <label className="login-form-label" htmlFor="login-form-email">Email, который подписан на рассылку:</label>
                     <div className="form-group login-form-email-group">
-                        <input type="email" className="form-control login-form-email-input" placeholder="Email" id="login-form-email" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                        <input type="email" className="form-control login-form-email-input" placeholder="Email" id="login-form-email" ref="email" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
                         <button type="submit" disabled={!this.state.email} className="btn btn-primary login-form-submit">Войти</button>
                     </div>
                     <p className="login-from-note">На почту придёт ссылка для входа в личный кабинет.</p>

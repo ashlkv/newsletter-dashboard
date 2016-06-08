840b53addba9ad78b90c4903a06ff4d99eac055c
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2016-06-08 15:25:19 +0300

    Auth redirection back and forth done right.

--- src/Auth.js
+++ src/Auth.js
@@ -58,40 +58,79 @@ let addTokenToQuery = function(query) {
 };
 
 /**
+ * Determines if we should try and authorize the user with backend auth request
+ * @returns {boolean}
+ */
+let authValidationRequired = function() {
+    // See if there is a token in the url (i.e. an attempt to authorize) or in local storage.
+    return !isAuthorized() && (getQueryToken() || getToken());
+};
+
+/**
+ * Makes auth request
+ * @returns {Promise}
+ */
+let validateAuth = function() {
+    return $.authorizedXHR({
+            url: '/backend/auth',
+            dataType: 'json',
+            cache: false
+        });
+};
+
+/**
+ * Handles login form enter: redirects authorized users to dashboard
+ * @param nextState
+ * @param replace
+ * @param callback
+ */
+let onLoginFormEnter = function(nextState, replace, callback) {
+    // If user is already authorized, redirect to dashboard
+    if (isAuthorized()) {
+        replace({
+            pathname: '/dashboard'
+        });
+        callback();
+    // If it is unclear if user is authorized, make an auth validation request, then decide.
+    } else if (authValidationRequired()) {
+        validateAuth().then(function() {
+            replace({
+                pathname: '/dashboard'
+            });
+            callback();
+        }).fail(function() {
+            // If auth validation failed, proceed to login form
+            callback();
+        });
+    // If user is not authorized, proceed to login form
+    } else {
+        callback();
+    }
+};
+
+/**
  * Handles restricted route enter. Called with a context different from Auth object.
  * @param nextState
  * @param replace
  * @param callback
  */
-// TODO Try to move auth attempt to root route onenter handler
 let onRestrictedRouteEnter = function(nextState, replace, callback) {
-    // See if there is a token in the url (i.e. an attempt to authorize) or in local storage.
-    let attemptAuthorize = getQueryToken() || getToken();
-
-    // If user is not authorized, but there is a token, request server auth.
-    if (!isAuthorized() && attemptAuthorize) {
-        $.authorizedXHR({
-            url: '/backend/auth',
-            dataType: 'json',
-            cache: false
-        })
-            .always(() => {
-                // If server auth is successful, proceed to the url, but without the token parameter
-                if (isAuthorized()) {
-                    // Do not allow authorized users on login page: redirect them to dashboard
-                    let pathname = /^\/?login/.test(nextState.location.pathname) ? '/dashboard' : nextState.location.pathname;
-                    replace({
-                        pathname: pathname
-                    });
-                // Otherwise, go to auth error page with a link to login form
-                } else {
-                    replace({
-                        pathname: '/auth-fail',
-                        state: {nextPathname: nextState.location.pathname}
-                    });
-                }
-                callback();
+    // If it is unclear if user is authorized, request server auth validation.
+    if (authValidationRequired()) {
+        validateAuth().then(function() {
+            // If server auth is successful, proceed to the url, but without the token parameter
+            replace({
+                pathname: nextState.location.pathname
             });
+            callback();
+        // If auth validation failed, go to auth error page with a link to login form
+        }).fail(function() {
+            replace({
+                pathname: '/auth-fail',
+                state: {nextPathname: nextState.location.pathname}
+            });
+            callback();
+        });
     // If there is no token in the url (i.e. no attempt to authorize), simply redirect to login page
     } else if (!isAuthorized()) {
         replace({
@@ -110,5 +149,6 @@ module.exports = {
     isAuthorized: isAuthorized,
     getToken: getToken,
     onRestrictedRouteEnter: onRestrictedRouteEnter,
+    onLoginFormEnter: onLoginFormEnter,
     addTokenToQuery: addTokenToQuery
 };
